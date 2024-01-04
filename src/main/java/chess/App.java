cd890c4c740f8039f18e25d4378dd85835c7c73d
Jacob Hobbs <JacobRayHobbs@gmail.com>
2024-01-04 15:15:13 -0600

    update2

--- src/main/java/chess/App.java
+++ src/main/java/chess/App.java
@@ -9,14 +9,19 @@ import javafx.stage.Stage;
  * */
 public class App extends Application {
 
+    /**
+     *  Overrides the start method of the Application superclass.
+     *  Instantiates new board object that utilizes startGame method of Board class.
+     * */
     @Override
     public void start(Stage stage) {
-        
         Board chessBoard = new Board();
         chessBoard.startGame(stage);
-        
     }
 
+    /**
+     * Main application method that launches new chess game at runtime.
+     * */
     public static void main(String[] args) {
         launch();
     }
