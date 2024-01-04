240002d54bc5b7daccde8c2941b9dae0da94b277
Jacob Hobbs <JacobRayHobbs@gmail.com>
2024-01-04 16:08:09 -0600

    Update for Player class
    Updated comments for methods

--- src/main/java/chess/Player.java
+++ src/main/java/chess/Player.java
@@ -2,25 +2,36 @@
 package chess;
 
 /**
- *
  * @author Jacob
  */
+
+/**
+ * Player class utilized to instantiate and define player objects.
+ * */
 public class Player {
     
     private String currentPlayer;
 
-    
+    /**
+     * Player constructor. Default currentPlayer set to white in keeping with
+     * rules of chess.
+     * */
     public Player() {
         this.currentPlayer = "WHITE";
     }
-    
+
+    /**
+     * Setter method for current player.
+     * */
     public void setCurrentPlayer(String player) {
         this.currentPlayer = player;
     }
-    
+
+    /**
+     * Getter method for current player.
+     * */
     public String getCurrentPlayer() {
         return this.currentPlayer;
     }
 
-    
 }
